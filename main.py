e319d807083f71be110b3d0c91da512df0c175ec
xmcp <f@bi.us>
2014-07-14 14:09:47 +0800

    v2.0

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import cherrypy
 from cherrypy.lib.static import serve_file
 from mako.template import Template
 import os
+import hashlib
 
 server_path='C:/users/xiao/desktop/shile'
 
 class shile:
     @cherrypy.expose
     def view(self,path):
+        try:
+            cherrypy.session['login']
+        except:
+            raise cherrypy.HTTPRedirect('/login')
         def origin(name):
             return name.replace('>','/')
         def urllike(name):
@@ -21,6 +26,44 @@ class shile:
 
     @cherrypy.expose
     def down(self,path):
+        try:
+            cherrypy.session['login']
+        except:
+            raise cherrypy.HTTPRedirect('/login')
         return serve_file(path.replace('>','/'),"application/x-download", "attachment")
+
+    @cherrypy.expose
+    def login(self,password=None):
+        if not password:
+            template = Template(filename=server_path+'/views/login.html',input_encoding='utf-8')
+            return template.render()
+        psw=hashlib.sha256()
+        psw.update(password.encode())
+        truepsw=hashlib.sha256()
+        truepsw.update(open('pass.txt','rb').read())
+        if truepsw.hexdigest()!=psw.hexdigest():
+            raise cherrypy.HTTPRedirect('/login')
+        else:
+            cherrypy.session['login']=True
+            raise cherrypy.HTTPRedirect('/')
+
+    @cherrypy.expose
+    def index(self):
+        try:
+            cherrypy.session['login']
+        except:
+            raise cherrypy.HTTPRedirect('/login')
+        else:
+            raise cherrypy.HTTPRedirect('/view/c:')
+
+    @cherrypy.expose
+    def logout(self):
+        try:
+            cherrypy.session['login']
+        except:
+            pass
+        else:
+            del cherrypy.session['login']
+        raise cherrypy.HTTPRedirect('/')
 
 cherrypy.quickstart(shile(),'','app.conf')
