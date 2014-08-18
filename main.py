0052741e2527837328c23b8bbdc340b5841a4004
xmcp <f@bi.us>
2014-08-18 11:28:36 +0800

    v6.0.02

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 #coding=utf-8
 
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import print_function
+
 import cherrypy
 from cherrypy.lib.static import serve_file
 from mako.template import Template
